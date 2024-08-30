5f4e1d6dfe1be7a03e644e0a3c0e06383123170c
PT-HUANG <qwe1593574268@gmail.com>
2024-08-30 18:46:35 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 //EXPRESS
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+//View => use Handlebar as view engine
+app.engine('.hbs', engine({extname: '.hbs'})) //更改附檔名.handlebars => .hbs
+app.set('view engine', '.hbs') //指定 view engine 為.hbs檔案
+app.set('views', './views')
+
+
+//Controller
 app.get('/', (req, res) => {
     res.redirect('/password-generator')
   })
   
 app.get('/password-generator', (req, res) => {
-    res.send('password generator')
+    res.render('index')
 })
 
 app.listen(port, () => {
